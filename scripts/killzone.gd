6d6a6f786bc2e8ca91910cb7917991a5367b7918
zhaoxm <hizhaoxm@gmail.com>
2026-07-15 11:55:29 +0800

    feat

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -5,8 +5,12 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("You Killed")
+	body.get_node("CollisionShape2D").queue_free()
+	body.get_node("AnimatedSprite2D").set_deferred("rotation", -90)
+	Engine.time_scale = .5
 	timer.start()
 
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
